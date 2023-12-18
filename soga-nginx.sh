226b2f1d24556883bf0653f899b565944486769b
sgmjwbg <61115527+sgmjwbg@users.noreply.github.com>
2023-12-19 02:17:25 +0800

    Update soga-nginx.sh

--- soga-nginx.sh
+++ soga-nginx.sh
@@ -272,7 +272,7 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=`curl -4s https://api.myip.la?d=${DOMAIN}`
+            resolve=`curl -sl 4s https://api.myip.la?d=${DOMAIN}`
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
